0165880c7db8f257aa0479a64483a3b859585c6a
Kevin <username.com>
2022-05-02 23:16:39 +0800

    ccy newest change

--- 鸽子喂料机/User/main.c
+++ 鸽子喂料机/User/main.c
@@ -25,16 +25,21 @@
 
 int16_t Set_Data,Set_Speed=20, Num;
 uint8_t Flag, Hcsr04_StartFlag;
-uint8_t Info1, Info2, Info3, Info4, State1;
+uint8_t Info1, Info2, Info3, Info4, State0;
 uint8_t X, Y, Value;
 float Distance1, Distance2, Distance3, Distance4;
 int16_t SpeedLeft=0,SpeedRight=0;
-int8_t LeftCalibration=1,RightCalibration=1,LeftInversion=1,RightInversion=1;
+int8_t LeftCalibration=1,RightCalibration=-1,LeftInversion=1,RightInversion=1;
 uint8_t DataReset=1;
+uint8_t State=1;
 
 void Speed(int16_t data);
 void Data_Analyse(void);
 void Distance_Get(void);
+void State1(void);
+void State2(void);
+void State3(void);
+void While_Init(void);
 
 int main(void)
 {	
@@ -66,11 +71,53 @@ int main(void)
 
 	while (1)
 	{
+			
+		While_Init();
+		
+		switch (State)
+		{
+			case SETTINGSTATE://下
+				State1();
+				break;
+			case CONTROLSTATE://中
+				State2();
+				break;
+			case DEBUGSTATE://上
+				State2();
+				break;
+		}
+		
+
+		
+		 DataReset++;
+		 DataReset%=20;
+	if(DataReset ==0)
+		 NRF24L01_DataReset(); //按键信息自动重置，防FNR通讯中断导致失控
+
+/*********************算法驱动************************************/
+		SpeedLeft=LeftInversion*LeftCalibration*SpeedConversion(SpeedLeft);
+		SpeedRight=RightInversion*RightCalibration*SpeedConversion(SpeedRight);
+
+			Robot_Move(SpeedLeft,SpeedRight);
+
+
+
+
+		LEDO_OFF();
+		
+		
+		
+	}
+}
+
+void While_Init()
+{
+	
 		Distance_Get();
 
 		Num ++;
 		OLED_ShowNum(1, 16, Num, 1);
-		State1 = NRF24L01_ReadByte(STATUS);
+		State0 = NRF24L01_ReadByte(STATUS);
 		Info1 = NRF24L01_ReadByte(EN_AA);
 		Info2 = NRF24L01_ReadByte(EN_RXADDR);
 		Info3 = NRF24L01_ReadByte(RX_ADDR_P0);
@@ -82,7 +129,7 @@ int main(void)
 		else
 			GPIO_SetBits(GPIOA, GPIO_Pin_8);
 		
-		OLED_ShowHexNum(4, 1, State1, 2);
+		OLED_ShowHexNum(4, 1, State0, 2);
 		OLED_ShowHexNum(4, 4, Info1, 2);
 		OLED_ShowHexNum(4, 7, Info2, 2);
 		OLED_ShowHexNum(4, 10, Info3, 2);
@@ -106,9 +153,14 @@ int main(void)
 		Data_Analyse();
 		
 		
-		
-/*********************状态1：摇杆检测************************************/		
-		NRF24L01_GetData(ROCKER_TRANSMIT);
+}
+
+void State1(void)
+{
+		if(NRF24L01_GetData(SWITCH_TRANSMIT)==SWITCH3_PIN2_NUM+SWITCH2_PIN1_NUM)	//拨钮开关05启用普通按键控制
+	{
+	/*********************摇杆检测************************************/
+	NRF24L01_GetData(ROCKER_TRANSMIT);
 
 		switch(Y)
 		{
@@ -134,41 +186,8 @@ int main(void)
 			case RIGHT_4:Speed(Set_Speed/2);RightInversion=-1;break;
 		
 		}
-
 		
-/*********************状态0：常规检测************************************/		
-		switch( NRF24L01_GetData(NORMAL_TRANSMIT))
-		{
-			case FEED_OFF:	//喂料器关
-				Relay_Set(ALL, RESET);
-			break;
-			case FEED_ON:	//喂料器开
-				Relay_Set(ALL, SET);
-			break;
-		}
-			
-		
-/*********************状态2：拨钮开关检测************************************/		
-		
-//			switch( NRF24L01_GetData(SWITCH_TRANSMIT))
-//		{
-////			case SWITCH3_PIN2_NUM:LeftCalibration=-1;RightCalibration=1;break;
-//			case SWITCH3_PIN2_NUM+SWITCH2_PIN1_NUM:LeftCalibration=1;RightCalibration=-1;break;//校准模式
-////			case SWITCH3_NONE:LeftCalibration=1;RightCalibration=1;break;
-////			case SWITCH3_NONE:LeftCalibration=-1;RightCalibration=-1;break;
-//				
-//		}
-		
-/*********************状态3：旋转编码器检测************************************/
-	if(NRF24L01_GetData(SWITCH_TRANSMIT)==SWITCH3_PIN2_NUM+SWITCH2_PIN1_NUM)	//拨钮开关05启用普通按键控制
-	{		
-		if(NRF24L01_GetData(KEY_TRANSMIT)==KEY_PIN4_NUM)
-		{
-			Set_Data=NRF24L01_GetData(ENCODER_TRANSMIT);
-			Speed(Set_Data);
-		}
-		
-/*********************状态4：按键检测************************************/
+		/********************按键检测************************************/
 	
 		switch(NRF24L01_GetData(KEY_TRANSMIT))
 		{
@@ -183,14 +202,12 @@ int main(void)
 		
 		}
 	}
+}
+
+void State2(void)
+{
 	
-			switch(NRF24L01_GetData(KEY_TRANSMIT))
-		{
-			case EmergencyFault	:	SpeedLeft=0;SpeedRight=0;break;//急停
-			case KEY_PIN1_NUM		:	TSDA_Order(LeftWheel,MotorStart);TSDA_Order(RightWheel,MotorStart);break;//启动
-			case KEY_PIN2_NUM		:	TSDA_Order(LeftWheel,MotorStop);TSDA_Order(RightWheel,MotorStop);break;//关闭
-			case KEY_PIN3_NUM		:	SpeedLeft=0;SpeedRight=0;break;//急停
-		}
+	
 		
 		/* 切换调试模式时，记得按下启动或者停止按键，以免切回普通模式导致保留上一次按键操作*/
 	if(NRF24L01_GetData(SWITCH_TRANSMIT)==SWITCH3_PIN2_NUM)//拨钮开关04启用校准模式,校准电机方向，摇杆可用	
@@ -203,31 +220,52 @@ int main(void)
 		}
 		
 		
-		DataReset++;
-		DataReset%=20;
-if(DataReset ==0)
-		 NRF24L01_DataReset(); //按键信息自动重置，防FNR通讯中断导致失控
+		/*********************旋转编码器检测************************************/
+	
+		if(NRF24L01_GetData(KEY_TRANSMIT)==KEY_PIN4_NUM)
+		{
+			Set_Data=NRF24L01_GetData(ENCODER_TRANSMIT);
+			Speed(Set_Data);
+		}
 
-/*********************状态5：算法驱动************************************/
-		SpeedLeft=LeftInversion*LeftCalibration*SpeedConversion(SpeedLeft);
-		SpeedRight=RightInversion*RightCalibration*SpeedConversion(SpeedRight);
-
-			Robot_Move(SpeedLeft,SpeedRight);
-//			TSDA_Data(LeftWheel,SpeedSetting,SpeedLeft);
-//		
-//			TSDA_Data(LeftWheel,SpeedSetting,SpeedLeft);		
-//			TSDA_Data(RightWheel,SpeedSetting,SpeedRight);
-
-//				Delay_ms(1000);
+	
+/*********************喂料************************************/		
+		switch( NRF24L01_GetData(NORMAL_TRANSMIT))
+		{
+			case FEED_OFF:	//喂料器关
+				Relay_Set(ALL, RESET);
+			break;
+			case FEED_ON:	//喂料器开
+				Relay_Set(ALL, SET);
+			break;
+		}
 		
 		
-		LEDO_OFF();
-		
+		switch(NRF24L01_GetData(KEY_TRANSMIT))
+		{
+			case EmergencyFault	:	SpeedLeft=0;SpeedRight=0;break;//急停
+			case KEY_PIN1_NUM		:	TSDA_Order(LeftWheel,MotorStart);TSDA_Order(RightWheel,MotorStart);break;//启动
+			case KEY_PIN2_NUM		:	TSDA_Order(LeftWheel,MotorStop);TSDA_Order(RightWheel,MotorStop);break;//关闭
+			case KEY_PIN3_NUM		:	SpeedLeft=0;SpeedRight=0;break;//急停
+		}
 		
 		
 	}
-}
 
+void State3(void)
+{
+	switch(NRF24L01_GetData(KEY_TRANSMIT))
+		{
+			case EmergencyFault	:	SpeedLeft=0;SpeedRight=0;break;//急停
+			case KEY_PIN1_NUM		:	TSDA_Order(LeftWheel,MotorStart);TSDA_Order(RightWheel,MotorStart);break;//启动
+			case KEY_PIN2_NUM		:	break;
+			case KEY_PIN3_NUM		:	SpeedLeft=0;SpeedRight=0;break;//急停
+			case KEY_PIN4_NUM		:	break;
+			case KEY_PIN5_NUM		:	break;
+			case KEY_PIN6_NUM		:	break;
+			case KEY_PIN7_NUM		:	break;
+		}
+}
 void Data_Analyse(void)
 {
 	
